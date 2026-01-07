052f2a919898189244b7854239ffcf9de2ed9e98
swastik <swastikdevidas252914@gmail.com>
2026-01-07 14:37:39 +0530

    Added DSA C++ programs

---
+++ 01_oddNumber.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    if(n % 2 != 0){
+        cout << n << " is an odd number." << endl;
+    } else {
+        cout << n << " is not an odd number." << endl;
+    }
+    return 0;
+}
