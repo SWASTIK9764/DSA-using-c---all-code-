f3610d288c6a94da5f7d686717f5c5e55aac07b6
swastik <swastikdevidas252914@gmail.com>
2026-01-11 15:41:00 +0530

    Print any table

---
+++ tableprinting.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num,i;
+    cout<<"Enter the number :";
+    cin>>num;
+    for(i=1;i<=10;i++){
+        cout<<num*i<<" ";
+
+    }
+    cout<<endl;
+}
