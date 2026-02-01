b0d688ad7d662edf22a0a1fc93e1399013a5ba45
swastik <swastikdevidas252914@gmail.com>
2026-02-01 23:11:54 +0530

    Create vote.cpp

---
+++ vote.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num;
+    cout<<"Enter the age of person:";
+    cin>>num;
+    if(num>=18){
+        cout<<"The person is elgible for vote";
+    }
+    else{
+        cout<<"Not elgibel for vote";
+    }
+}
