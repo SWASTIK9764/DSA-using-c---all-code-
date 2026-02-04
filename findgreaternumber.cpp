39b8183208541afd9984136db9501208b6d88e29
swastik <swastikdevidas252914@gmail.com>
2026-02-04 23:08:39 +0530

    Create findgreaternumber.cpp

---
+++ findgreaternumber.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num1,num2;
+    cout<<"Enter the first number:";
+    cin>>num1;
+    cout<<"Enter the second number:";
+    cin>>num2;
+    if(num1>num2){
+        cout<<"Number one is greater then second ";
+    }
+    else if(num1==num2){
+        cout<<"Number is zero or negative number";
+    }
+    else{
+        cout<<"Number second is greater then first:";
+    }
+
+
+}
