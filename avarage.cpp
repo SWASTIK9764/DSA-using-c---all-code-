7fa261ca41cb1ddca6ce47fc65ba865b2383cc48
swastik <swastikdevidas252914@gmail.com>
2026-02-05 22:52:12 +0530

    Create avarage.cpp

---
+++ avarage.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+// average of two numbers
+int main() {
+    int num1,num2,result;
+    cout<<"Enter the first number:";
+    cin>>num1;
+    cout<<"Enter the second number:";
+    cin>>num2;
+    result=num1/num2;
+    cout<<"Averge of number:"<<result;
+    return 0;
+}
