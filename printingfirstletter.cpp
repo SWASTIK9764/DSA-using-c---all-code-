60943eef3c99639bfe1ee3fc23e81dd20f5a6810
swastik <swastikdevidas252914@gmail.com>
2026-02-15 23:36:59 +0530

    Create printingfirstletter.cpp

---
+++ printingfirstletter.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+// when char data type is used then only one char is print
+int main() {
+    char name;
+    cout<<"Enter your name:";
+    cin>>name;
+    cout<<name;
+
+    return 0;
+}
